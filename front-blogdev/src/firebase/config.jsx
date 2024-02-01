b1a99a9586a856535fe1d425a454d035130fa0cd
Francisco <54285612+FranciscoCAJr@users.noreply.github.com>
2024-02-01 17:00:09 -0500

    Update config.jsx

--- front-blogdev/src/firebase/config.jsx
+++ front-blogdev/src/firebase/config.jsx
@@ -2,13 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCh0cU1K7KmzramuSmUaaRMYr9lB2ZC91Y",
-  authDomain: "blogdev--gogeta.firebaseapp.com",
-  projectId: "blogdev--gogeta",
-  storageBucket: "blogdev--gogeta.appspot.com",
-  messagingSenderId: "642008196852",
-  appId: "1:642008196852:web:99e9100932c695f2d720a1",
-  measurementId: "G-7TLX8B2S9K"
 };
 
 const app = initializeApp(firebaseConfig);
